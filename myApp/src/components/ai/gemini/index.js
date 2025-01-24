fbf6497b0fb65bd111243efb1617250cede55bc8
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2025-01-24 23:00:34 +0530

    adds .env

--- myApp/src/components/ai/gemini/index.js
+++ myApp/src/components/ai/gemini/index.js
@@ -1,6 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const GEMINI_API_KEY = "AIzaSyD5nmtycYxwry-fsDedeuHaDM1M0N8rOm4";
+const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+console.log(GEMINI_API_KEY);
 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 const GeminiModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
